3d419b9c3e6f26f9e7ac06e6d26b4fd490be2c92
elmeeee <37185272+elmeeee@users.noreply.github.com>
2025-12-15 16:28:27 +0700

    Initial Commit

---
+++ CoreML-Vision/CoreML_VisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreML_VisionApp.swift
+//  CoreML-Vision
+//
+//  Created by Elmee on 15/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct CoreML_VisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoreML-VisionTests/CoreML_VisionTests.swift
@@ -0,0 +1,17 @@
+//
+//  CoreML_VisionTests.swift
+//  CoreML-VisionTests
+//
+//  Created by Elmee on 15/12/2025.
+//
+
+import Testing
+@testable import CoreML_Vision
+
+struct CoreML_VisionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
